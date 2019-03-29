9903a16a092c1873ac4d261e5918e31be38cdaa7
huangyong <huangyong@guahao.com>
2019-03-29 10:01:56 +0800

    提交

--- src/test/java/org/slipover/frame/jquery/test/MainTest.java
+++ src/test/java/org/slipover/frame/jquery/test/MainTest.java
@@ -8,9 +8,7 @@ import static org.slipover.frame.jquery.JQuery.$;
 public class MainTest {
 
     public static void main(String[] args) {
-        System.out.println($.get("http://his-member-service-qa4.guahao-test.com/member/package/info/150798805128880128", httpHeaders -> {
-            httpHeaders.setContentType(MediaType.APPLICATION_JSON_UTF8);
-        }));
+
     }
 
 }
